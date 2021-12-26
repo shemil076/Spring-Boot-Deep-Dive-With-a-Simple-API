457292f05bf1bbe825bb114d33053ed8260e63a7
Pramuditha Karunarathna <shemil076@gmail.com>
2021-12-26 16:14:30 +0530

    Advance application Configuration-Type safe configuration propeerties

--- src/main/java/com/in28minutes/springboot/WelcomeController.java
+++ src/main/java/com/in28minutes/springboot/WelcomeController.java
@@ -1,19 +1,37 @@
 package com.in28minutes.springboot;
 
 import com.in28minutes.springboot.WelcomeService;
+import com.in28minutes.springboot.configuration.BasicConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class WelcomeController {
 
     @Autowired
     private WelcomeService service;
+
+    @Autowired
+    private BasicConfiguration configuration;
+
     @RequestMapping("/welcome")
     public String welcome() {
         return service.retrieveWelcome();
+    }
+
+    @RequestMapping("/dynamic-configuration")
+    public Map dynamicConfiguration() {
+        Map map = new HashMap();
+
+        map.put("message", configuration.getMessage());
+        map.put("number", configuration.getNumber());
+        map.put("value", configuration.isValue());
+        return map;
     }
 }
 
