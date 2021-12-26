bf855e2b8fd6ddcc831b5c465d303d066e629db1
Pramuditha Karunarathna <shemil076@gmail.com>
2021-12-26 13:31:52 +0530

    Create a Service to add a new question to survey: @PostMapping, Postman

--- src/main/java/com/in28minutes/springboot/WelcomeService.java
+++ src/main/java/com/in28minutes/springboot/WelcomeService.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class WelcomeService{
     public String retrieveWelcome() {
-        return "Good Morning";
+        return "Good Morning ";
     }
 }
--- src/main/java/com/in28minutes/springboot/controller/SurveyController.java
+++ src/main/java/com/in28minutes/springboot/controller/SurveyController.java
@@ -4,10 +4,11 @@ import com.in28minutes.springboot.model.Question;
 import com.in28minutes.springboot.model.Survey;
 import com.in28minutes.springboot.service.SurveyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,6 +22,21 @@ public class SurveyController {
     public List<Question> retrieveQuestionsForSurvey(@PathVariable String surveyId) {
        return surveyService.retrieveQuestions(surveyId);
     }
+
+    @PostMapping("/surveys/{surveyId}/questions")
+    public ResponseEntity<Void> addQuestionsToSurvey(@PathVariable String surveyId, @RequestBody Question newQuestion) {
+
+        Question question = surveyService.addQuestion(surveyId, newQuestion);
+
+        if(question == null){
+            return ResponseEntity.noContent().build();
+        }
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(question.getId()).toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+
     @GetMapping("/surveys/{surveyId}/questions/{questionId}")
     public Question retrieveDetailsForQuestion(@PathVariable String surveyId,@PathVariable String questionId) {
         return surveyService.retrieveQuestion(surveyId,questionId);
