17dafdf057c1641bf23f35758f2d329b617bc706
Pramuditha Karunarathna <shemil076@gmail.com>
2021-12-26 14:58:50 +0530

    Adding Dynamic Configuration to your application: YAML

--- src/main/java/com/in28minutes/springboot/WelcomeService.java
+++ src/main/java/com/in28minutes/springboot/WelcomeService.java
@@ -1,11 +1,16 @@
 package com.in28minutes.springboot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WelcomeService{
+
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+
     public String retrieveWelcome() {
-        return "Good Morning ";
+        return welcomeMessage;
     }
 }
