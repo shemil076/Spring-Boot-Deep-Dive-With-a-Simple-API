7f8926ac1ed9a95efe3673f0b5746791c469ad82
Pramuditha Karunarathna <shemil076@gmail.com>
2021-12-26 16:45:04 +0530

    Spring Boot Starter: Spring Data JPA with CommandLineRunner

---
+++ src/main/java/com/in28minutes/springboot/jpa/User.java
@@ -0,0 +1,49 @@
+package com.in28minutes.springboot.jpa;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String name;
+    private String role;
+
+    protected  User(){}
+
+    public User(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
---
+++ src/main/java/com/in28minutes/springboot/jpa/UserRepository.java
@@ -0,0 +1,11 @@
+package com.in28minutes.springboot.jpa;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.io.Serializable;
+
+//@Repository
+public interface UserRepository extends   CrudRepository<User, Long> {
+
+}
